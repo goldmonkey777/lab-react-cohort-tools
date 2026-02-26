fb6a3f7af0a2e0b81a1d366a40d820ef38a0e9d1
goldmonkey777 <contact@goldmonkey.studio>
2026-02-26 13:06:38 +0100

    done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import StudentCard from "../components/StudentCard";
+import { Link } from "react-router-dom";
 
 import studentsData from "../assets/students.json";
 
@@ -21,7 +22,9 @@ function HomePage() {
       {students &&
         students.map((student) => {
           return (
+            <Link key={student._id} to={`/students/${student._id}`}>
               <StudentCard key={student._id} {...student} />
+            </Link>
           );
         })}
     </div>
